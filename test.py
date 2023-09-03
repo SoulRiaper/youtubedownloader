c7bae45de6151c1bd7f83a5ed48ca31f7de5a901
SoulRiaper <soulriaper@gmail.com>
2023-09-03 16:07:29 +0300

    delete some trash

--- test.py
+++
@@ -1,5 +0,0 @@
-from pytube import YouTube
-
-video = YouTube("https://youtu.be/VKio7bKURlc?si=l6oqEd6B4Sk8xCob")
-
-video.streams.get_audio_only().download("video.mp4", filename=f"{video.title.replace(' ', '_')}.mp3")
